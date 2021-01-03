04bb765c3b065c36640045308aa00acd5e156c13
elios-cama <elios.cama@gmail.com>
2021-01-03 22:30:34 +0100

    finished the bronze medal

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = '6A7D1E90-CC41-4754-B512-BBE32D877B39';
+const coinURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+
+class CoinData {
+  Future getCoinData() async{
+    String URLrequest = '$coinURL/BTC/EUR?apikey=$apiKey';
+    http.Response response = await http.get(URLrequest);
+
+    if(response.statusCode == 200){
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    }else{
+      print(response.statusCode);
+      throw 'Problem in the request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+import 'dart:convert';
+
+import 'coin_data.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,17 +15,59 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String SelectedCurrency = 'USD';
 
-  List<DropdownMenuItem> getDropdownItems() {
+  DropdownButton<String> androidDropDown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
     for (String currency in currenciesList) {
-      var newItem = DropdownMenuItem(child: Text(currency),value: currency,);
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
       dropdownItems.add(newItem);
     }
-    return dropdownItems;
+    return DropdownButton<String>(
+        value: SelectedCurrency,
+        items: dropdownItems,
+        onChanged: (value) {
+          setState(() {
+            SelectedCurrency = value;
+          });
+        });
   }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: null,
+      children: pickerItems,
+    );
+  }
+
+  String rate = '?';
+
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      //13. We can't await in a setState(). So you have to separate it out into two steps.
+      setState(() {
+        rate = data.toStringAsFixed(1);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
+  void initState(){
+    super.initState();
+    getData();
+  }
   Widget build(BuildContext context) {
-    getDropdownItems();
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -39,7 +87,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $rate EUR',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -50,19 +98,12 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: SelectedCurrency,
-              items: getDropdownItems(),
-              onChanged: (value){
-              setState(() {
-                SelectedCurrency = value;
-              });
-            }),
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidDropDown(),
+          )
         ],
       ),
     );
